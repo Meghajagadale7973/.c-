9219dffeec52d1af4492da718b583c42aeac3a6b
Meghajagadale7973 <jagadalemegha7973@gmail.com>
2022-05-24 16:10:22 +0530

    third commit

--- ConsoleApp10/ConsoleApp10/Program.cs
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -6,7 +6,25 @@ namespace ConsoleApp10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String str = "";
+            Console.WriteLine(" enter string");
+            str = Console.ReadLine();
+            char[] array = str.ToCharArray();
+            Array.Reverse(array);
+            String str_reverse = new string(array);
+            if (str.Equals(str_reverse))
+            {
+                Console.WriteLine("its a pelindrome string");
+            }
+            else
+            {
+                Console.WriteLine("its not pelindrome");
+
+            }
+            Console.ReadLine();
+
+             
+       
         }
     }
 }
