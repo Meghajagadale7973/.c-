35e5b6ddfb49414138c01a873f1645faf8048f3a
Meghajagadale7973 <jagadalemegha7973@gmail.com>
2022-05-25 15:57:29 +0530

    initial commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        static void Main(string [] args)
+        {
+            int i, fact = 1;
+            Console.WriteLine("enter number");
+             int number= Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.Write("FACTORIAL OF " + number + "is:" + fact);
+
+        }
+    }
+}
